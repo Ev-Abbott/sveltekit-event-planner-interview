1d35e06361b8d4a924c71715df29389a35eb85f0
Ev Abbott <evmakesthings@gmail.com>
2025-03-20 11:52:21 -0700

    feat: Implements streaming for single event data page

--- src/routes/[eventId]/+page.svelte
+++ src/routes/[eventId]/+page.svelte
@@ -1,14 +1,22 @@
 <script lang="ts">
-	import type { PageData } from "./$types";
+	import { page } from '$app/state';
+	import type { PageData } from './$types';
 
-    let {data}: {data: PageData} = $props();
+	let { data }: { data: PageData } = $props();
 </script>
 
-
 <div>
-    {#if data.event}
-        <h2 class="text-lg font-bold">{data.event.id}: {data.event.title}</h2>
-        <p>{data.event.description}</p>
-        <p>{data.event.date}</p>
-    {/if}
+	{#await data.event}
+		<p>Loading...</p>
+	{:then event}
+		{#if event}
+			<h2 class="text-lg font-bold">{event.id}: {event.title}</h2>
+			<p>{event.description}</p>
+			<p>{event.date}</p>
+		{:else}
+			<p>Sorry, there is no event for id: {page.params.eventId}</p>
+		{/if}
+	{:catch}
+		<p>Sorry, something went wrong when fetching event for id: {page.params.eventId}</p>
+	{/await}
 </div>
