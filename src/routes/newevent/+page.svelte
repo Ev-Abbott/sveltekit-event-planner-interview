e392b017d5e24ca77da1e32dabcf918e8d056883
Ev Abbott <evmakesthings@gmail.com>
2025-03-20 12:03:38 -0700

    feat: Implements basic progressive enhancement to form, provides loading indicator

--- src/routes/newevent/+page.svelte
+++ src/routes/newevent/+page.svelte
@@ -1,13 +1,36 @@
 <script lang="ts">
+	import { enhance } from '$app/forms';
 
+	let isCreating = $state(false);
 </script>
 
-<form method="POST">
-    <!-- form for creating new event -->
-    <label for="title">Title</label>
-    <input type="text" id="title" name="title" required>
-    <textarea id="description" name="description" rows="4" cols="50" placeholder="Description"></textarea>
-    <label for="date">Date</label>
-    <input type="datetime-local" id="date" name="date" required>
-    <button type="submit">Create Event</button>
+<form
+	method="POST"
+	use:enhance={() => {
+		isCreating = true;
+
+		return async ({ update }) => {
+			await update();
+			isCreating = false;
+		};
+	}}
+>
+	<!-- form for creating new event -->
+	<label for="title">Title</label>
+	<input type="text" id="title" name="title" required disabled={isCreating} />
+	<textarea
+		id="description"
+		name="description"
+		rows="4"
+		cols="50"
+		placeholder="Description"
+		disabled={isCreating}
+	></textarea>
+	<label for="date">Date</label>
+	<input type="datetime-local" id="date" name="date" required disabled={isCreating} />
+	<button type="submit">Create Event</button>
 </form>
+
+{#if isCreating}
+	<p>Saving Event... please wait</p>
+{/if}
